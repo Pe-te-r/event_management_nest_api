31c07411d16203e680d3ec72f47823aebb4c9f71
Pe-te-r <mburupeter8526@gmail.com>
2025-05-27 14:01:31 +0300

    all resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { EventRegistrationsModule } from './event_registrations/event_registrations.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    EventsModule,
+    EventRegistrationsModule,
+    FeedbackModule,
+    PaymentsModule,
+  ],
   controllers: [],
   providers: [],
 })
