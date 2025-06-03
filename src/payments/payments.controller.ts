fe78f9b8a1a9d78df73ff034f57e5c18dfd98075
Pe-te-r <mburupeter8526@gmail.com>
2025-06-03 16:13:03 +0300

    more on payment swagger

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -11,32 +11,40 @@ import {
 import { PaymentsService } from './payments.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post()
+  @ApiOperation({summary:'create payments'})
   create(@Body() createPaymentDto: CreatePaymentDto) {
     return this.paymentsService.create(createPaymentDto);
   }
 
   @Get()
+  @ApiOperation({summary:'get all payments'})
+  @ApiQuery({type:'detailed',required:false,description:'Toggle to get more details on payments'})
   findAll(@Query('detailed') detailed: boolean) {
     return this.paymentsService.findAll(detailed);
   }
-
+  
   @Get(':id')
+  @ApiOperation({summary:'get payment by id'})
+  @ApiQuery({type:'detailed',required:false,description:'Toggle to get more details on payment'})
   findOne(@Param('id') id: string,@Query('detailed') detailed:boolean) {
     return this.paymentsService.findOne(id,detailed);
   }
-
+  
   @Patch(':id')
+  @ApiOperation({summary:'update payment by id'})
   update(@Param('id') id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
     return this.paymentsService.update(+id, updatePaymentDto);
   }
-
+  
   @Delete(':id')
+  @ApiOperation({summary:'delete payment by id'})
   remove(@Param('id') id: string) {
     return this.paymentsService.remove(+id);
   }
