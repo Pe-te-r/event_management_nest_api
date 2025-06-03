0ab91c38e40ac9f8d88a1c12416e57e57f4657cf
Pe-te-r <mburupeter8526@gmail.com>
2025-06-03 16:15:51 +0300

    more on feedback swagger

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -11,35 +11,43 @@ import {
 import { FeedbackService } from './feedback.service';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 import { UpdateFeedbackDto } from './dto/update-feedback.dto';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('feedbacks')
 export class FeedbackController {
   constructor(private readonly feedbackService: FeedbackService) {}
 
   @Post()
+  @ApiOperation({summary:'create feedback'})
   create(@Body() createFeedbackDto: CreateFeedbackDto) {
     return this.feedbackService.create(createFeedbackDto);
   }
-
+  
   @Get()
+  @ApiOperation({ summary: 'get all feedbacks' })
+  @ApiQuery({type:'boolean',name:'detailed',required:false})
   findAll(@Query('detailed') detailed:boolean) {
     return this.feedbackService.findAll(detailed);
   }
-
+  
   @Get(':id')
+  @ApiOperation({ summary: 'get feedbacks by id' })
+  @ApiQuery({type:'boolean',name:'detailed',required:false})
   findOne(@Param('id') id: string,@Query('detailed') detailed:boolean) {
     return this.feedbackService.findOne(id,detailed);
   }
-
+  
   @Patch(':id')
+  @ApiOperation({ summary: 'update feedbacks by id' })
   update(
     @Param('id') id: string,
     @Body() updateFeedbackDto: UpdateFeedbackDto,
   ) {
     return this.feedbackService.update(+id, updateFeedbackDto);
   }
-
+  
   @Delete(':id')
+  @ApiOperation({ summary: 'delete feedbacks by id' })
   remove(@Param('id') id: string) {
     return this.feedbackService.remove(+id);
   }
